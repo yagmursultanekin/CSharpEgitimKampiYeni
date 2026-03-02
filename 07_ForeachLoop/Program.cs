ebb7c34fe3468567a3459d14aa4a3b0385995dfb
yagmursultanekin <163584968+yagmursultanekin@users.noreply.github.com>
2026-03-02 17:35:10 +0300

    Foreach döngüsü ile ilgili çalışma yapıldı.

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  Foreach Döngüsü
+
+            // Foreach(1;2;3;4)
+
+            //string[] cities = { "Tunceli", "İzmir", "Manisa", "Ankara" };
+
+            //foreach(string city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+
+            //int[] numbers = {1,2,3,4,5,6,7};
+
+            //foreach(int sayi in numbers)
+            //{
+            //    Console.WriteLine(sayi);
+            //}
+
+
+            //int[] numbers = { 35, 42, 1000, 500, 3, 2, 0 };
+
+            //foreach(int sayi in numbers)
+            //{
+            //    if (sayi % 2 == 0)
+            //    {
+            //        Console.WriteLine(sayi);
+            //    }
+            //}
+
+
+            //int[] numbers = { 35, 42, 1000, 500, 3, 2, 0 };
+
+            //int total = 0;
+            //foreach(int i in numbers)
+            //{
+            //    total += i;
+            //}
+
+            //Console.WriteLine(total);
+
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,3,5,8
+            //};
+
+            //foreach(int number in numbers) 
+            //{ 
+            //Console.WriteLine(number);
+            //}
+
+
+            //string word = "Merhaba";
+
+            //foreach(char c in word)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            #endregion
+
+            
+
+            Console.Read();
+        }
+    }
+}
