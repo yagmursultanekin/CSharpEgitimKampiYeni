ad9de6429cbb4baa6fe8e68bcc2d9547560e3a0d
yagmursultanekin <163584968+yagmursultanekin@users.noreply.github.com>
2026-03-06 15:46:03 +0300

    CRUD işlemlerinde çalışma tamamlandı.

--- 10_DatabaseCrud/Program.cs
+++ 10_DatabaseCrud/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -55,7 +56,72 @@ namespace _10_DatabaseCrud
 
             #endregion
 
+            #region Ürün Listelem İşlemi
 
+            //SqlConnection connection = new SqlConnection("Data source=.;initial catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("select * from Tblproduct", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable=new DataTable();
+            //adapter.Fill(dataTable);
+
+            //foreach(DataRow row in dataTable.Rows)
+            //{
+            //    foreach(var item in row.ItemArray)
+            //    {
+            //        Console.Write(item.ToString() + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //connection.Close();
+
+            #endregion
+
+            #region Ürün Silme İşlemi
+
+            //Console.Write("Silinecek ürün Id: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data source=.;initial catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Delete from TblProduct Where ProductId=@productId", connection);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("Silme işlemi başarıyla yapıldı!");
+
+            #endregion
+
+            #region Ürün Güncelleme İşlemi
+
+            //Console.Write("Güncellenecek Ürün Id: ");
+            //int productId=int.Parse(Console.ReadLine());
+            
+            //Console.Write("Güncellenecek Ürün Adı: ");
+            //string productName = Console.ReadLine();
+           
+            //Console.Write("Güncellenecek Ürün Fiyatı: ");
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+
+
+            //SqlConnection connection = new SqlConnection("Data source=.;initial catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("update TblProduct set ProductName=@productName,ProductPrice=@productPrice where ProductId=@productId", connection);
+
+            //command.Parameters.AddWithValue("@productName" , productName);
+            //command.Parameters.AddWithValue("@productPrice" , productPrice);
+            //command.Parameters.AddWithValue("@productId" , productId);
+            //command.ExecuteNonQuery();
+            
+            //connection.Close();
+
+            //Console.WriteLine("Güncelleme işlemi başarıyla tamamlandı");
+
+            #endregion
 
 
             Console.Read();
